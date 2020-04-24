ded2004d247d730ea8fb2f6942559d74913ab951
iamdempa <banukajananathjayarathna@gmail.com>
2020-04-25 02:55:24 +0530

    test

---
+++ test1/lib/pages/first_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:test1/camera/camera_screen.dart';
+
+class FirstScreen extends StatefulWidget {
+  FirstScreen({Key key}) : super(key: key);
+
+  @override
+  _FirstScreenState createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        leading: new Icon(Icons.live_tv),
+        title: const Text('SnapNews'),
+        backgroundColor: Colors.red,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.help),
+            onPressed: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (_) => CameraScreen()));
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text("Welcome to SnapNews - IT17157124"),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        elevation: 4.0,
+        icon: const Icon(Icons.camera_enhance),
+        label: const Text('Snap'),
+        backgroundColor: Colors.red,
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (_) => CameraScreen()),
+          );
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        child: new Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(null),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(null),
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
